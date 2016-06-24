a5fc99b1531783b0c7cebc6599a284075e22404e
Bennett Grigull <grigullb@kenyon.edu>
2016-06-24 13:16:04 -0700

    Update question.rb

--- question.rb
+++ question.rb
@@ -1,1 +1,25 @@
+class Question
+	def initiate
+		@num1 = nil
+		@num2 = nil
+		@answer = nil
+		@new = nil
+	end
 
+	attr_reader :answer
+	attr_accessor :new
+
+  def generate_new
+		@num1 = random_number
+		@num2 = random_number
+		@answer = @num1 + @num2
+		@new = "What does #{@num1} + #{@num2} equal?"
+	end
+
+
+	private
+
+	def random_number
+		number = rand(21)
+  end 
+end
